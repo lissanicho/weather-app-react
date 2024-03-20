688a1af9464298226614a62f849e54ce4c51c80b
lissanicho <143479341+lissanicho@users.noreply.github.com>
2024-03-20 17:35:08 +1300

    including real data

--- src/CurrentForecast.js
+++ src/CurrentForecast.js
@@ -1,29 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+
 
 export default function CurrentForecast() {
-  return (
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  function handleResponse(response){
+    console.log (response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      description: response.data.weather[0].description,
+    });
+    setReady(true);
+  }
+  if (ready) {return (
     <div>
       <div class="row" id="current-forecast">
         <div class="col-6">
           
           <ul>
-            <li id="cityName">City</li>
-          <li id="date">Day and time</li>
+            <li className="cityName">{weatherData.city}</li>
+          <li className="date">Day and time</li>
           <li>
-            <span id="temp"> current temp </span>
+            <span className="temp"> {Math.round(weatherData.temperature)}°C</span>
           </li>
-          <li id="cloud-condition">cloud condition</li>
+          <li className="cloud-condition">{weatherData.description}</li>
           </ul>
         </div>
         <div class="col-6">
           <ul>
-            <li><img src="" alt="cloudy" id="icon" /></li>
-          <li id="humidity">humidity</li>
-          <li id="wind">wind</li>
+            <li><img src={weatherData.iconUrl} alt={weatherData.description} id="icon" />{weatherData.icon}</li>
+          <li className="humidity">{weatherData.humidity}% humidity</li>
+          <li className="wind">{weatherData.wind}km/h</li>
           </ul>
           
         </div>
       </div>
     </div>
-  );
+  );}
+  
+
+  else{
+  
+  let city= "London";
+  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=17ad6e67aa629189f73b053634668b20&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "Loading";
+  }
 }
